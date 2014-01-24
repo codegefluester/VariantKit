586563be33e7165ac9c25337cd31380038806884
codegefluester <bjoern@codegefluester.de>
2014-01-24 10:46:40 +0100

    Added class files

---
+++ VariantKit.h
@@ -0,0 +1,13 @@
+//
+//  VariantKit.h
+//  VariantKit
+//
+//  Created by Björn Kaiser on 24.01.14.
+//  Copyright (c) 2014 Björn Kaiser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VariantKit : NSObject
+
+@end
---
+++ VariantKit.m
@@ -0,0 +1,13 @@
+//
+//  VariantKit.m
+//  VariantKit
+//
+//  Created by Björn Kaiser on 24.01.14.
+//  Copyright (c) 2014 Björn Kaiser. All rights reserved.
+//
+
+#import "VariantKit.h"
+
+@implementation VariantKit
+
+@end
